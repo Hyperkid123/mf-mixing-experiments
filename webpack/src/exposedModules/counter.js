3fcea4c76c224c35e02af513885b77ca9ebf5da4
Martin Marosi <marvusm.mmi@gmail.com>
2024-01-18 13:07:50 +0100

    Add basic exposed module to webpack.

---
+++ webpack/src/exposedModules/counter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Counter = () => {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <h1>Webpack Counter</h1>
+      <div>
+        <button onClick={() => setCount((count) => count + 1)}>Add</button>
+      </div>
+      <div>
+        <p>
+          <strong>Count:</strong> {count}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,21 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('@module-federation/enhanced');
+
+const MFPlugin = new ModuleFederationPlugin({
+  name: 'WebpackApp',
+  filename: 'webpack-entry.js',
+  library: {
+    type: 'global',
+    name: 'WebpackApp',
+  },
+  exposes: {
+    './WebpackCounter': './src/exposedModules/counter.js',
+  },
+  shared: {
+    react: { singleton: true },
+    'react-dom': { singleton: true },
+  },
+});
 
 const htmlPlugin = new HtmlWebpackPlugin({
   template: './index.html',
@@ -25,7 +42,7 @@ module.exports = {
       },
     ],
   },
-  plugins: [htmlPlugin],
+  plugins: [htmlPlugin, MFPlugin],
   devServer: {
     port: 8001,
   },

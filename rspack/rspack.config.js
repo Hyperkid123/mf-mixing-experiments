f4a4695f26c904c493748411f6de0f12d72bebe5
Martin Marosi <marvusm.mmi@gmail.com>
2024-01-18 12:27:42 +0100

    Setup base rspack react app.

---
+++ rspack/rspack.config.js
@@ -0,0 +1,55 @@
+const rspack = require("@rspack/core");
+
+const HTMLPlugin = new rspack.HtmlRspackPlugin({
+  template: './index.html',
+})
+
+
+/** @type { import("rspack").Configuration } */
+module.exports = {
+  mode: "development",
+  entry: {
+    main: {
+      import: ["./src/index.js"],
+    },
+  },
+  output: {
+    publicPath: "/",
+  },
+  devServer: {
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /[\\/]node_modules[\\/]/,
+        loader: 'builtin:swc-loader',
+        options: {
+          sourceMap: true,
+          jsc: {
+            parser: {
+              syntax: 'ecmascript',
+              jsx: true,
+            },
+          },
+        },
+      },
+    ],
+    parser: {
+      asset: {
+        dataUrlCondition: {
+          maxSize: 1,
+        },
+      },
+    },
+  },
+  plugins: [HTMLPlugin],
+  builtins: {
+    progress: {},
+    react: {
+      development: true,
+      refresh: true,
+    },
+  },
+};
---
+++ rspack/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+      There will be dragons
+    </div>      
+  );
+}
+
+export default App;
